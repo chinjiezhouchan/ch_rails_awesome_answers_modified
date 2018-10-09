66dbc0d10016db6aa8a2a9420da182cafecb0dcf
Tammam Kbeili <tammam.kbeili@gmail.com>
2018-10-09 09:31:16 -0700

    add welcome controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+
+  def index
+    # by Convention: Rails will attempt to render a template inside the views folder
+    # within a subfolder that matches the controller name (in this case `welcome`)
+    # using a template that matches the actions name (in this case `index`), the defult
+    # extension will be `.html.erb`
+    # this will be: app/views/welcome/index.html.erb
+
+    # render plain: 'Hello World'
+  end
+
+  def home_page
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # this defined a routing rule that says: if I recevie an HTTP / GET request
+  # to URL: `/hello_world` then handle this with the `WelcomeContrller` using the `index`
+  # method (called action, methods inside controller are called actions)
+  get('/hello_world', { to: 'welcome#index', as: :hello_world })
+  
+  # the `as` option gives a special name to the URL so we can easily generate
+  # it in our view and controller files, to generate url we use the name we chose
+  # here after adding `_path` or `_url` to it (for relative or absolute path)
+  get('/', { to: 'welcome#home_page', as: :home_page })
 end
